f4e9bf8cccb9d70e2d88d2bd20b53dbc03e7b9c7
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-10-23 20:10:28 +0500

    login form half

--- husnain_app/urls.py
+++ husnain_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact_us, name="contact_us"),
+    path('login/', views.sgn_in, name="signin"),
 ]
--- husnain_app/views.py
+++ husnain_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 from .models import Student
 
@@ -10,3 +11,16 @@ def index(request):
 
 def contact_us(request):
     return render(request, 'contact_us.html')
+
+
+def sgn_in(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        pwd = request.POST.get('pwd')
+        user = authenticate(request, username=username, password=pwd)
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return redirect(request.path)
+    return render(request, "login_page.html")
